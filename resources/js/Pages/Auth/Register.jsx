a3c3676a5f9667bf0ba355a9ed85c39107fe701b
yehiaalkashf-sys <yehiaalkashf@gmail.com>
2025-08-25 17:50:23 -0700

    Remove unnecessary files

---
+++ resources/js/Pages/Auth/Register.jsx
@@ -0,0 +1,144 @@
+import InputError from '@/Components/InputError';
+import InputLabel from '@/Components/InputLabel';
+import PrimaryButton from '@/Components/PrimaryButton';
+import TextInput from '@/Components/TextInput';
+import GuestLayout from '@/Layouts/GuestLayout';
+import { Head, Link, useForm } from '@inertiajs/react';
+import { useContext } from 'react';
+import { ThemeContext } from '../../Contexts/ThemeContext';
+import Header from '../Components/Header';
+import Footer from '../Components/Footer';
+
+export default function Register() {
+    const { theme } = useContext(ThemeContext);
+
+    const { data, setData, post, processing, errors, reset } = useForm({
+        name: '',
+        email: '',
+        password: '',
+        password_confirmation: '',
+    });
+
+    const submit = (e) => {
+        e.preventDefault();
+
+        post(route('register'), {
+            onFinish: () => reset('password', 'password_confirmation'),
+        });
+    };
+
+    return (
+        <>
+        <Header />
+        <GuestLayout>
+            <Head title="Register" />
+
+            <form
+                onSubmit={submit}
+                className={`rounded-xl p-8 shadow-xl transition duration-500 ${
+                    theme === "dark"
+                        ? "bg-gray-800 text-gray-700"
+                        : "bg-white text-gray-800"
+                }`}
+            >
+                <h2 className="text-2xl font-bold mb-6 text-center">Create a New Account</h2>
+
+                <div>
+                    <InputLabel htmlFor="name" value="Name" />
+
+                    <TextInput
+                        id="name"
+                        name="name"
+                        value={data.name}
+                        className="mt-1 block w-full"
+                        autoComplete="name"
+                        isFocused={true}
+                        onChange={(e) => setData('name', e.target.value)}
+                        required
+                        placeholder="Enter your name"
+                    />
+
+                    <InputError message={errors.name} className="mt-2" />
+                </div>
+
+                <div className="mt-4">
+                    <InputLabel htmlFor="email" value="Email Address" />
+
+                    <TextInput
+                        id="email"
+                        type="email"
+                        name="email"
+                        value={data.email}
+                        className="mt-1 block w-full"
+                        autoComplete="username"
+                        onChange={(e) => setData('email', e.target.value)}
+                        required
+                        placeholder="Enter your email"
+                    />
+
+                    <InputError message={errors.email} className="mt-2" />
+                </div>
+
+                <div className="mt-4">
+                    <InputLabel htmlFor="password" value="Password" />
+
+                    <TextInput
+                        id="password"
+                        type="password"
+                        name="password"
+                        value={data.password}
+                        className="mt-1 block w-full"
+                        autoComplete="new-password"
+                        onChange={(e) => setData('password', e.target.value)}
+                        required
+                        placeholder="Enter your password"
+                    />
+
+                    <InputError message={errors.password} className="mt-2" />
+                </div>
+
+                <div className="mt-4">
+                    <InputLabel htmlFor="password_confirmation" value="Confirm Password" />
+
+                    <TextInput
+                        id="password_confirmation"
+                        type="password"
+                        name="password_confirmation"
+                        value={data.password_confirmation}
+                        className="mt-1 block w-full"
+                        autoComplete="new-password"
+                        onChange={(e) =>
+                            setData('password_confirmation', e.target.value)
+                        }
+                        required
+                        placeholder="Re-enter your password"
+                    />
+
+                    <InputError
+                        message={errors.password_confirmation}
+                        className="mt-2"
+                    />
+                </div>
+
+                <div className="mt-6 flex items-center justify-between">
+                    <Link
+                        href={route('login')}
+                        className={`text-sm underline focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 ${
+                            theme === "dark"
+                                ? "text-gray-300 hover:text-white"
+                                : "text-gray-600 hover:text-gray-900"
+                        }`}
+                    >
+                        Already have an account?
+                    </Link>
+
+                    <PrimaryButton className="ms-4" disabled={processing}>
+                        Register
+                    </PrimaryButton>
+                </div>
+            </form>
+        </GuestLayout>
+        <Footer />
+        </>
+    );
+}
