78e2e75a7f3c9aaf18fdcb2e7f12b5c2868e2699
yehiaalkashf-sys <yehiaalkashf@gmail.com>
2025-09-04 02:58:43 -0700

    finnaly

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $next($request);
+
+        $allowedOrigins = [
+            'https://medinesteeldoor.com',
+            'https://www.medinesteeldoor.com'
+        ];
+
+        $origin = $request->headers->get('Origin');
+
+        if ($origin && in_array($origin, $allowedOrigins)) {
+            $response->headers->set('Access-Control-Allow-Origin', $origin);
+            $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, X-CSRF-TOKEN');
+            $response->headers->set('Access-Control-Allow-Credentials', 'true');
+        }
+
+        return $response;
+    }
+}
