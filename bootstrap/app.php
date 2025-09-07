70273a24392d59d2b48bd682b3b0d995d70dc4b0
yehiaalkashf-sys <yehiaalkashf@gmail.com>
2025-09-07 04:47:08 -0700

    main

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,6 +22,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->alias([
             'admin' => IsAdmin::class,
+            'cors' => \App\Http\Middleware\CorsMiddleware::class,
         ]);
 
     $middleware->api(append: [
