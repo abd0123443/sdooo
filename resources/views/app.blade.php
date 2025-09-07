d934d873998c5fe3e7ea8f5816c05ddab5cf6a5e
yehiaalkashf-sys <yehiaalkashf@gmail.com>
2025-09-06 18:46:17 -0700

    1

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Medina Steel Doors - Building Entrance Doors</title>
+        <title>Medine Steel Doors - Building Entrance Doors</title>
         <meta name="description" content="Medina Steel Doors provides secure, durable, and stylish entrance doors for residential and commercial buildings." />
         <meta name="keywords" content="building entrance doors, commercial steel doors, residential entrance doors, secure entry doors, Medina Steel Doors" />
         <link rel="icon" type="image/png" href="{{ asset('icon.jpeg') }}">
