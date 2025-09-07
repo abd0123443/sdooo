1b4ebae24edf8a33aceb5b5aececfc29cf5ea9c8
yehiaalkashf-sys <yehiaalkashf@gmail.com>
2025-09-07 06:13:40 -0700

    1

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,8 +7,8 @@
         <title>Medine Steel Doors - Building Entrance Doors</title>
         <meta name="description" content="Medina Steel Doors provides secure, durable, and stylish entrance doors for residential and commercial buildings." />
         <meta name="keywords" content="building entrance doors, commercial steel doors, residential entrance doors, secure entry doors, Medina Steel Doors" />
-<link rel="icon" type="image/x-icon" href="{{ asset('favicon-v2.ico') }}">        <meta name="google-site-verification" content="lE4DeKO3BYNJYGI3H4RjYdk2Q2deCp-Vaz26F3mOunk" />
-        <!-- Fonts -->
+        <link rel="icon" type="image/x-icon" href="{{ asset('favicon-v2.ico') }}">
+        <meta name="google-site-verification" content="ONHGjT5h85DTI55eb_wt_nZz8PNX1wYsIzjpPUvaskY" />
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@400;700&display=swap" rel="stylesheet">
